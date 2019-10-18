633b7c92e6e113d2a7740b601788baf982bcb1e3
sang <576606705@qq.com>
2019-10-18 22:40:29 +0800

    init project

---
+++ src/main/java/com/alsace/simplejava/SimpleJavaApplication.java
@@ -0,0 +1,21 @@
+package com.alsace.simplejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author sangmingming
+ * @since 2019/10/18 0018
+ */
+@SpringBootApplication
+public class SimpleJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alsace/simplejava/SimpleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsace.simplejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
